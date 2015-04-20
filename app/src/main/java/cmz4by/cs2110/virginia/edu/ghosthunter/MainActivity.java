5be64dbb236f9a036f9cf17ccc424cea0b4c5722
Gardner Fiveash <jg5ash4@gmail.com>
2015-04-20 18:07:32 -0400

    ghost moves and bounces off screen edges

--- app/src/main/java/cmz4by/cs2110/virginia/edu/ghosthunter/MainActivity.java
+++ app/src/main/java/cmz4by/cs2110/virginia/edu/ghosthunter/MainActivity.java
@@ -1,98 +1,33 @@
 package cmz4by.cs2110.virginia.edu.ghosthunter;
 
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.opengl.GLSurfaceView;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.Window;
-import android.widget.ImageView;
+
+        import android.support.v7.app.ActionBarActivity;
+        import android.os.Bundle;
+        import android.text.Layout;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.widget.ImageView;
+        import android.view.View;
+        import android.os.Bundle;
+        import android.view.Window;
 
 
 public class MainActivity extends ActionBarActivity {
-    public int weapon;
-    public String direction;
-
-  //  private GLSurfaceView mGLView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
 
-        // Create a GLSurfaceView instance and
-        // set as the ContentView for Activity
-       // mGLView = new MyGLSurfaceView(this);
-        setContentView(R.layout.activity_main);
-        ImageView ghostIcon = (ImageView) findViewById(R.id.ghost);
-        ghostIcon.setImageResource(R.drawable.chillghost);
-    }
-
-    class MyGLSurfaceView extends GLSurfaceView {
-       // private final MyGLSurfaceView mRenderer;
-
-        public MyGLSurfaceView(Context context) {
-            super(context);
-
-            // Create an OpenGL ES2.0 context
-            setEGLContextClientVersion(2);
-            //mRenderer = new MyGLRenderer();
-
-           // setRenderer(mRenderer);
-
-            // Render the view only when there is a
-            // change in the drawing data
-            setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
-            }
+        setContentView(new GameView(this));
     }
 
 
-    public void turnUp(View view) {
-        ImageView image = (ImageView) findViewById(R.id.player);
-        direction = "up";
-
-        if(weapon == 1) image.setImageResource(R.drawable.poliwag_back_spear);
-        else image.setImageResource(R.drawable.poliwag_back);
-    }
-
-    public void turnLeft(View view) {
-        ImageView image = (ImageView) findViewById(R.id.player);
-        direction = "left";
-
-        if(weapon == 1) image.setImageResource(R.drawable.poliwag_left_spear);
-        else image.setImageResource(R.drawable.poliwag_left);
-    }
-
-    public void turnRight(View view) {
-        ImageView image = (ImageView) findViewById(R.id.player);
-        direction = "right";
-
-        if(weapon == 1) image.setImageResource(R.drawable.poliwag_right_spear);
-        else image.setImageResource(R.drawable.poliwag_right);
-    }
-
-    public void turnDown(View view) {
-        ImageView image = (ImageView) findViewById(R.id.player);
-        direction = "down";
-
-        if(weapon == 1) image.setImageResource(R.drawable.poliwag_front_spear);
-        else image.setImageResource(R.drawable.poliwag_front);
-
-    }
-
-    public void giveSpear(View view) {
-        ImageView image = (ImageView) findViewById(R.id.player);
-        weapon = 1;
-        if(direction.equals("up")) image.setImageResource(R.drawable.poliwag_back_spear);
-        else if(direction.equals("left")) image.setImageResource(R.drawable.poliwag_left_spear);
-        else if(direction.equals("right")) image.setImageResource(R.drawable.poliwag_right_spear);
-        else if(direction.equals("down")) image.setImageResource(R.drawable.poliwag_front_spear);
-        else image.setImageResource(R.drawable.poliwag_back_spear);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     @Override
@@ -110,3 +45,4 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
