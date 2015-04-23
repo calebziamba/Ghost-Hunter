4522865053419bf2380a378ac4d4176fdaecb5fe
Gardner Fiveash <jg5ash4@gmail.com>
2015-04-23 17:29:39 -0400

    halfway through bombs

---
+++ app/src/main/java/cmz4by/cs2110/virginia/edu/ghosthunter/Bomb.java
@@ -0,0 +1,11 @@
+package cmz4by.cs2110.virginia.edu.ghosthunter;
+
+import java.util.List;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+public class Bomb {
+   private float x;
+   private float y;
+
+}
